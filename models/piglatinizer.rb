1631acf864de14ee51f8db496e8da31d6facdbaa
Keith Isham <kisham8899@gmail.com>
2019-08-31 14:06:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ end
   def piglatinize_word(word)
     fl = word[0].downcase 
     
-    if "a", "e", "i", "o", "u"].include?(fl)
+    if ["a", "e", "i", "o", "u"].include?(fl)
       "#{word}way"
       #pigs a word starting with a-u
     else 
