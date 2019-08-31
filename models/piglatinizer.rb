e0e79b242da99a7b22a031e98cc3404f8ece90f4
Keith Isham <kisham8899@gmail.com>
2019-08-31 13:57:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,6 +3,7 @@ class PigLatinizer
   def piglatinize(string)
     aWord = string.split(" ")
     bWord = aWord.map {|word| piglatinize_word(word) }
+    bWord.joi
 end
   
   def piglatinize_word(word)
